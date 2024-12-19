2684ccbf944ef655f6c469e7b61a40fce139cc6f
XJJ <2799248108@qq.com>
2024-12-19 12:55:29 +0800

    Site updated: 2024-12-19 12:55:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/18/quickSort/","2024/12/18/my-first-blog/"];function toRandomPost(){
+var posts=["2024/12/18/my-first-blog/","2024/12/18/quickSort/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
