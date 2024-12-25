47eea441672a4051805a1ceb8545b93d67404531
XJJ <2799248108@qq.com>
2024-12-25 22:04:20 +0800

    Site updated: 2024-12-25 22:04:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/25/Git/","2024/12/22/HeapSort/","2024/12/22/MergeSort/","2024/12/18/QuickSort-0/","2024/12/22/ShellSort/","2024/12/23/嵌入式原理/","2024/12/25/管道、环境变量及其常用命令/","2024/12/23/设计模式/"];function toRandomPost(){
+var posts=["2024/12/25/Git/","2024/12/22/HeapSort/","2024/12/22/MergeSort/","2024/12/18/QuickSort-0/","2024/12/22/ShellSort/","2024/12/23/嵌入式原理/","2024/12/23/设计模式/","2024/12/25/管道、环境变量及其常用命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
